3417629a6ac0b36539c8878ed1be9daa533867b4
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-12-13 12:29:26 +0100

    no message

--- src/main/scala/com/mogobiz/notify/config/MogonotifyHandlers.scala
+++ src/main/scala/com/mogobiz/notify/config/MogonotifyHandlers.scala
@@ -7,7 +7,6 @@ package com.mogobiz.notify.config
 import com.mogobiz.notify.handlers.NotificationHandler
 
 /**
- * Created by hayssams on 25/07/14.
  */
 object MogonotifyHandlers {
   val notificationHandler = new NotificationHandler
