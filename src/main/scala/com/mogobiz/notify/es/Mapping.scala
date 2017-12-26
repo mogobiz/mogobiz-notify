de945549170488f60a11fd29686ab130a8ee5b69
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2017-12-26 11:28:34 +0200

    scalariformat

--- src/main/scala/com/mogobiz/notify/es/Mapping.scala
+++ src/main/scala/com/mogobiz/notify/es/Mapping.scala
@@ -4,41 +4,62 @@
 
 package com.mogobiz.notify.es
 
-import akka.actor.ActorSystem
+import java.io.InputStream
+
+import akka.actor.{ActorSystem, Terminated}
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
 import com.mogobiz.es.EsClient
 import com.mogobiz.notify.config.Settings
+import com.mogobiz.utils.HttpRequestor
 import com.sksamuel.elastic4s.http.ElasticDsl._
-import spray.client.pipelining._
-import spray.http._
+import com.sksamuel.elastic4s.http.index.admin.DeleteIndexResponse
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 object Mapping {
-  def clear = EsClient().execute(deleteIndex(Settings.Notification.EsIndex)).await
+  def clear: DeleteIndexResponse =
+    EsClient().execute(deleteIndex(Settings.Notification.EsIndex)).await
 
   def mappingNames = List()
 
-  def set() {
-    def route(url: String)       = "http://" + com.mogobiz.es.Settings.ElasticSearch.FullUrl + url
-    def mappingFor(name: String) = getClass().getResourceAsStream(s"es/notify/mappings/$name.json")
+  def set(): Future[Terminated] = {
+    def route(url: String) =
+      "http://" + com.mogobiz.es.Settings.ElasticSearch.FullUrl + url
 
-    implicit val system: ActorSystem                                   = akka.actor.ActorSystem("mogopay-boot")
-    val pipeline: HttpRequest => scala.concurrent.Future[HttpResponse] = sendReceive
+    def mappingFor(name: String): InputStream =
+      getClass().getResourceAsStream(s"es/notify/mappings/$name.json")
+
+    implicit val system: ActorSystem = akka.actor.ActorSystem("mogopay-boot")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
 
     mappingNames foreach { name =>
-      val url     = s"/${Settings.Notification.EsIndex}/$name/_mapping"
+      val url = s"/${Settings.Notification.EsIndex}/$name/_mapping"
       val mapping = scala.io.Source.fromInputStream(mappingFor(name)).mkString
-      val x: Future[Any] = pipeline(Post(route(url), mapping)) map { response: HttpResponse =>
-        response.status match {
-          case StatusCodes.OK => System.err.println(s"The mapping for `$name` was successfully set.")
-          case _              => System.err.println(s"Error while setting the mapping for `$name`: ${response.entity.asString}")
-        }
+      val request = HttpRequest(
+        method = HttpMethods.POST,
+        uri = Uri(route(url)),
+        entity = HttpEntity(MediaTypes.`application/json`, mapping)
+      )
+
+      val x: Future[Any] = HttpRequestor.doRequest(request) map {
+        response: HttpResponse =>
+          response.status match {
+            case StatusCodes.OK =>
+              System.err.println(
+                s"The mapping for `$name` was successfully set.")
+            case _ =>
+              Unmarshal(response.entity).to[String].map { data =>
+                System.err.println(
+                  s"Error while setting the mapping for `$name`: $data")
+              }
+          }
       }
       Await.result(x, 10 seconds)
     }
 
-    system.shutdown()
+    system.terminate()
   }
 }
